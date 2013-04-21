2b37c15474fff9345acb16dc03f1acd797bdb4af
Kham <kham@kham-K53BY.(none)>
2013-04-21 16:15:10 +0200

    some product validations added

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,10 @@ class Product
   
   include ActiveModel::Validations
 
-  attr_accessor :title
+  attr_accessor :title, :description, :price, :photo
 
   validates_presence_of :title
+  validates_presence_of :description
+  validates_format_of :price, with: /\A[^-]\d+\.\d{2}\Z/
+  validates_format_of :photo, with: /\A(http|https):\/\/(www\.)?\w+\.\w+\/\w+\Z/, allow_blank: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,17 +7,61 @@ describe Product do
 	context "to be valid" do
     before(:each) do
       @product = Product.new
+      @product.price = '19.43'
+      @product.description = 'Product description'
+      @product.title = 'Product title'
     end
-    
-    it "A product must have a title, description, and price." do
-      @product.title = ''
-      @product.should_not be_valid
+
+    context "A product must have" do 
+
+      it "a title" do
+        @product.title = ''
+        @product.should_not be_valid
+        @product.title = 'Product title'
+        @product.should be_valid
+      end
+
+      it "description" do 
+       @product.description = ''
+       @product.should_not be_valid
+       @product.description = 'Product description'
+       @product.should be_valid
+     end
+
+     it "price" do
+      @product.should_not be_nil
     end
-    
-    
-    it "The title and description cannot be empty strings."
-    it "The title must be unique for all products in the system"
-    it "The price must be a valid decimal numeric value and greater than zero"
-    it "The photo is optional. If present it must be a valid URL format."
-  end
+
+
+    it "The title must be unique for all products in the system" do
+     pending 'after implementing db' 
+   end
+
+
+   it "The price must be a valid decimal numeric value and greater than zero" do
+     @product.price = ''
+     @product.should_not be_valid
+     @product.price = 'shabada'
+     @product.should_not be_valid
+     @product.price = '19'
+     @product.should_not be_valid
+     @product.price = '19.432'
+     @product.should_not be_valid
+     @product.price = -10.01
+     @product.should_not be_valid
+     @product.price = '19.43'
+     @product.should be_valid
+   end
+
+   it "The photo is optional. If present it must be a valid URL format." do
+     @product.photo.should be_nil 
+     @product.should be_valid
+     @product.photo = 'shabada'
+     @product.should_not be_valid
+     @product.photo = 'http://google.pl/image'
+     @product.should be_valid
+   end
+
+ end
+end
 end
